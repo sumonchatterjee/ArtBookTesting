64480243dd5a6a297871d4d0a506a3d24cf3ff80
sumon88 <sumon@livelike.com>
2022-10-18 00:10:23 +0530

    fragment test added with mockito and espresso

---
+++ app/src/androidTest/java/com/example/artbooktesting/ui/ArtFragmentTest.kt
@@ -0,0 +1,53 @@
+package com.example.artbooktesting.ui
+
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.filters.MediumTest
+import com.example.artbooktesting.R
+import com.example.artbooktesting.util.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+import javax.inject.Inject
+
+@MediumTest
+@HiltAndroidTest
+class ArtFragmentTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var fragmentFactory:ArtFragmentFactory
+
+
+    @Before
+    fun setUp(){
+        hiltRule.inject()
+    }
+
+
+    @Test
+    fun `testNavigation-from-art-to-artdetails`(){
+
+        val navController = Mockito.mock(NavController::class.java)
+        launchFragmentInHiltContainer<ArtFragment>(
+             factory = fragmentFactory
+        ){
+              Navigation.setViewNavController(requireView(),navController)
+        }
+
+        Espresso.onView(ViewMatchers.withId(R.id.fav)).perform(click())
+        Mockito.verify(navController).navigate(
+            ArtFragmentDirections.actionArtFragmentToArtDetailsFragment()
+        )
+    }
+
+
+}
